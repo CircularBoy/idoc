14c5c606eba7ed3c0ce162020c2c7ec893bd8bdb
Shrike <--yurets097@gmail.com>
2019-08-02 16:10:23 +0300

    Revert "Revert "new gulp and observer for map""
    This reverts commit dba1b7ac4fd4d8393898d510e52bb656092a47c5.

---
+++ app/libs/webp/webp.js
@@ -0,0 +1,5 @@
+var i=new Image();
+i.onload=i.onerror=function(){
+  document.body.classList.add(i.height?"webp":"no-webp");
+};
+i.src="data:image/webp;base64,UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==";
